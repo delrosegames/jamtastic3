862971b14e92ec5850670c74d52841ee2107a536
Fernando Henrique Rosa <ferhrosa@gmail.com>
2018-02-18 16:17:28 -0300

    Export do protótipo inicial

---
+++ docs/export/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1518981412,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tilemap_floor.png",
+		"images/tilemap_obj_blocking.png",
+		"images/testchar-sheet0.png",
+		"images/testchar-sheet1.png",
+		"images/testchar-sheet2.png",
+		"images/testcharbase-sheet0.png",
+		"images/pontos.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
